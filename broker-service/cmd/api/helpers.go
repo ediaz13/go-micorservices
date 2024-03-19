ffd4aa1881184269ce9d176b04bb31faacee29d2
ediaz13 <diazemanuel27@gmail.com>
2024-03-18 21:16:29 -0300

    Creating some helper errorJSON

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -26,7 +26,7 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
-		return errors.New("Body must have only a single JSON value")
+		return errors.New("body must have only a single json value")
 	}
 
 	return nil
@@ -54,4 +54,18 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 
 	return nil
 
+}
+
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(w, statusCode, payload)
 }
